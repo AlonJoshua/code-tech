49dac730750d6a8b68f738f5140d736615211eb1
Alon Joshua <alonjoshua@gmail.com>
2020-04-05 16:17:39 +0300

    Fetching and storing request in state works

--- src/App.js
+++ src/App.js
@@ -5,16 +5,60 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
+    console.log('constructor');
+      this.state = {
+      students: [
+            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+         ]
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMout');
+    this.fetchData();
+  }
+
+  fetchData = async function () {
+    const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+    ]
+      try {
+      const requestArray = []
+      const arrayOfPromises = await urls.map(url => fetch(url))
+      for await (let request of arrayOfPromises) {
+        const data = await request.json();
+        const newArray = await requestArray.push(data)
+        console.log('requestArray: ', requestArray)
+      }
+      this.setState({ 
+          users: requestArray[0],
+          posts: requestArray[1],
+          albums: requestArray[2]
+        });
+      console.log('new state ', this.state);
+    }
+      catch (err) {
+      console.log(`we had an error: ${err}`);
+    }
   }
 
   render() {
+    console.log('render');
     return (
       <Fragment>
       <h1 className='titles'>This App is for code testing</h1>
       {/* AsyncAwait Section */}
       <h2 className='titles'>async await</h2>
       <h3 className='titles'>fetching urls and review the data</h3>
-      <TableCard />
+      <TableCard
+      students={this.state.students}
+       />
+      }
       </Fragment>
     )
   }
--- src/containers/TableCard.js
+++ src/containers/TableCard.js
@@ -3,43 +3,17 @@ import React, { Component } from 'react';
 class TableCard extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			students: [
-            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-         ]
-		};
-	}
-
-	fetchData = async function () {
-		const urls = [
-		'https://jsonplaceholder.typicode.com/users',
-		'https://jsonplaceholder.typicode.com/posts',
-		'https://jsonplaceholder.typicode.com/albums'
-	]
-		  try {
-			const arrayOfPromises = urls.map(url => fetch(url))
-			for await (let request of arrayOfPromises) {
-				const data = await request.json();
-				this.setState(data);
-			}
-		}
-		  catch (err) {
-			const error = `we had an error ${err}`;
-		}
 	}
 
 	renderTableHeader() {
-		let header = Object.keys(this.state.students[0]);
+		let header = Object.keys(this.props.students[0]);
 		return header.map((key,i) => {
 			return <th key={i}>{key.toUpperCase()}</th>
 		})
 	}
 
 	renderTableData() {
-		return this.state.students.map((student, i) => {
+		return this.props.students.map((student, i) => {
 			const { id, name, age, email } = student;
 			return (
 				<tr key={id}>
